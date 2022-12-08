de535d7d73598e0151f67ffe66b402378720caf0
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-08 14:16:21 -0800

    Create Capstone.py

---
+++ Capstone.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Predict MLB At-Bat Outcomes⚾️")
+
+def main_page():    
+    from PIL import Image 
+    image1 = Image.open('Images/mlb.png')
+    st.image(image1)
+
+    st.sidebar.markdown("# Welcome!⚾️")
+    st.sidebar.markdown(" ")
+    if st.sidebar.checkbox(" Select For Help 🔍"):
+        st.sidebar.info("This is the welcome page which describes how to interact with the different pages and the purpose of the Streamlit app.")
+        st.sidebar.markdown("### Above ⬆ is a drop down of different pages to navigate through. Select the page you are interested in exploring.")
+        
+        
+        
+        
+page_names_to_funcs = {
+    "Welcome Page": main_page,
+    
+    }
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
