3b42ad9b9ec60526c4ed0a3a3c3a056d3cc1b7e8
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-11 13:58:34 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -46,8 +46,7 @@ def main_page():
     s = spraychart(a, 'braves', title='Dansby Swanson vs Max Scherser', colorby='player')
     fig= Figure()
     fig.set_size_inches(10, 8, forward = True)
-    axes = fig.subplots(nrows = 2, ncols = 2)
-    spraychart(a, 'braves', title='Dansby Swanson vs Max Scherser', colorby='player', ax = axes[0,0])
+    spraychart(a, 'braves', title='Dansby Swanson vs Max Scherser', colorby='player')
     st.pyplot(fig)
     
     
