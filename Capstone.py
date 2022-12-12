bc31fa077202a152aa0f4cc16dcc7e3c788dc82c
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 02:23:11 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -128,7 +128,6 @@ def page2():
                     braves = Image.open('Images/braves logo.png')
                     st.image(braves)
                     st.write("")
-                    st.write(" ")
     else:
         alteam = st.radio("Select an American League Team.", ('Blue Jays', 'Orioles', 'Rays', 'Red Sox', 'Yankees', 'Guardians', 'Royals', 'Tigers', 'Twins', 'White Sox', 'Angels', 'Astros', 'Athletics', 'Mariners', 'Rangers'))
         if alteam == "Blue Jays":
