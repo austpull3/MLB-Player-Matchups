baa3040a81e4c1d8e8c460efd24a010d99ed2682
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 00:43:08 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -114,6 +114,34 @@ def page2():
             unsafe_allow_html=True
             )
     add_bg_from_local('Images/tmobile.jpeg')
+    from shiny import App, render, ui
+
+    # Import modules for plot rendering
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    app_ui = ui.page_fluid(
+        ui.layout_sidebar(
+            ui.panel_sidebar(
+                ui.input_slider("n", "N", 0, 100, 20),
+            ),
+            ui.panel_main(
+                ui.output_plot("histogram"),
+            ),
+        ),
+    )
+
+
+    def server(input, output, session):
+        @output
+        @render.plot(alt="A histogram")
+        def histogram():
+            np.random.seed(19680801)
+            x = 100 + 15 * np.random.randn(437)
+            plt.hist(x, input.n(), density=True)
+
+
+    app = App(app_ui, server, debug=True)
 
    
         
