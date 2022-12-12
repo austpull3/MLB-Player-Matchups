5a3b15915d24910f19e8007f7b6b3eec5b690a4e
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 02:14:24 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -120,6 +120,10 @@ def page2():
     if league == "NL":
         st.write("You selected the National League.")
         nlteam = st.radio("Select a National League Team.", ('Braves', 'Marlins', 'Mets', 'Nationals', ' Phillies', 'Brewers', 'Cardinals', 'Cubs', 'Pirates', 'Reds', 'D-backs', 'Dodgers', 'Giants', 'Padres', 'Rockies'))
+        if nlteam == "Braves":
+            from PIL import Image 
+            braves = Image.open('Images/braves logo.png')
+            st.image(braves)
     else:
         alteam = st.radio("Select an American League Team.", ('Blue Jays', 'Orioles', 'Rays', 'Red Sox', 'Yankees', 'Guardians', 'Royals', 'Tigers', 'Twins', 'White Sox', 'Angels', 'Astros', 'Athletics', 'Mariners', 'Rangers'))
         if alteam == "Blue Jays":
