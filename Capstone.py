3499343649de794dbf979cefcf313b348fbe9b32
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 02:27:24 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -127,7 +127,7 @@ def page2():
                     from PIL import Image 
                     braves = Image.open('Images/braves logo.png')
                     st.image(braves)
-            elif if nlteam == "Marlins":
+            elif nlteam == "Marlins":
                 with col5:
                 from PIL import Image 
                 marlins = Image.open('Images/marlins.png')
