01bffbdff6b65aff39814d1e330da173d22f8cd1
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 02:27:46 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -129,9 +129,9 @@ def page2():
                     st.image(braves)
             elif nlteam == "Marlins":
                 with col5:
-                from PIL import Image 
-                marlins = Image.open('Images/marlins.png')
-                st.image(marlins)
+                    from PIL import Image 
+                    marlins = Image.open('Images/marlins.png')
+                    st.image(marlins)
             
     else:
         alteam = st.radio("Select an American League Team.", ('Blue Jays', 'Orioles', 'Rays', 'Red Sox', 'Yankees', 'Guardians', 'Royals', 'Tigers', 'Twins', 'White Sox', 'Angels', 'Astros', 'Athletics', 'Mariners', 'Rangers'))
