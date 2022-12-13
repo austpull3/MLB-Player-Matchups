f7f545962050c0e902f05691eebe888c6abeee02
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 20:47:34 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -178,8 +178,13 @@ def page2():
                 hitter = 665489
             else:
                 hitter = 606192
-        playerlook = st.text_input(" Last Name, First Name")
-        st.write(playerid_lookup(playerlook))
+        
+        player_name = st.text_input('Enter a player name:')
+
+        if player_name:
+            player_info = playerid_lookup(player_name)
+            st.write(player_info)
+        
         pitcherdiv = st.radio("Select pitcher from the AL or NL.", ('AL', 'NL'))
         if pitcherdiv == 'AL':
                pitcheral = st.selectbox("Please select an AL pitcher.", ['Tyler Wells: BAL (R)', 'Drew Rasmussen: TB (R)', 'Nick Pivetta: BOS (R)', 'Nestor Cortes: NYY (L)', 'Triston McKenzie: CLE (R)', 'Brady Singer: KC (R)', 'Tarik Skubal: DET (L)', 'Sonny Gray: MIN (R)', 'Dylan Cease: CWS (R)', 'Shohei Ohtani: LAA (R)', 'Justin Verlander: HOU (R)', 'James Kaprielian: OAK (R)', 'Chris Flexen: SEA (R)', 'Martin Perez: TEX (L)'])
