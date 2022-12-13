e61ca37bc4808465831833da362f2abb6b6f5527
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 21:18:53 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -223,23 +223,6 @@ def page2():
                pitcheral = st.selectbox("Please select an AL pitcher.", ['Tyler Wells: BAL (R)', 'Drew Rasmussen: TB (R)', 'Nick Pivetta: BOS (R)', 'Nestor Cortes: NYY (L)', 'Triston McKenzie: CLE (R)', 'Brady Singer: KC (R)', 'Tarik Skubal: DET (L)', 'Sonny Gray: MIN (R)', 'Dylan Cease: CWS (R)', 'Shohei Ohtani: LAA (R)', 'Justin Verlander: HOU (R)', 'James Kaprielian: OAK (R)', 'Chris Flexen: SEA (R)', 'Martin Perez: TEX (L)'])
         else:
            pitchernl = st.selectbox("Please select an NL pitcher.", ['Max Fried: ATL (L)', 'Sandy Alcantara: MIA (R)', 'Max Scherzer: NYM (R)', 'Paolo Espino: WSH (R)', 'Aaron Nola: PHI (R)', 'Corbin Burnes: MIL (R)', 'Miles Mikolas: STL (R)', 'Marcus Stroman: CHC (R)', 'Jose Quintana: PIT (L)', 'Nick Lodolo: CIN (L)', 'Zac Gallen: ARI (R)', 'Clayton Kershaw: LAD (L)', 'Carlos Rodon: SF (L)', 'Blake Snell: SD (L)', 'Ryan Feltner: COL (R)'])
-        
-    
-    
-
-    st.title("Pybaseball Spraychart")
-
-    # Get the spraychart data for a specific player
-    player_name = st.text_input('Enter player name:')
-    data = statcast_batter('2021-01-01', '2021-12-31', 514888)
-    s = spraychart(data, 'astros')
-    fig = s.figure
-    # Display the spraychart
-    st.pyplot(fig)
-    tot = data.events.value_counts()
-    st.write(tot)
-    fig2 = px.bar(data, x = 'events', y = "pitch_type", animation_frame = "game_date", animation_group = "events")
-    st.write(fig2)
 
 page_names_to_funcs = {
     "Welcome Page": main_page,
