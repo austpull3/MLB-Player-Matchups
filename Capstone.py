bf79b4131742be9eb575e466c0db0eeda7385f2d
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-13 19:01:12 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -38,123 +38,7 @@ def main_page():
     
 def page2():
     st.title("Explore MLB Data and visualize spraycharts of your favorite players ⚾️") 
-    league = st.radio("Select National League or American League:", ("NL", "AL"))
-    if league == "NL":
-        col4, col5, col6 = st.columns(3)
-        with col4:
-            st.write("You selected the National League.")
-            nlteam = st.radio("Select a National League Team.", ('Braves', 'Marlins', 'Mets', 'Nationals', 'Phillies', 'Brewers', 'Cardinals', 'Cubs', 'Pirates', 'Reds', 'D-backs', 'Dodgers', 'Giants', 'Padres', 'Rockies'))
-            if nlteam == "Braves":
-                with col5:
-                    from PIL import Image 
-                    braves = Image.open('Images/braves logo.png')
-                    st.image(braves)
-            elif nlteam == "Marlins":
-                with col5:
-                    from PIL import Image 
-                    marlins = Image.open('Images/marlins.png')
-                    st.image(marlins)
-            elif nlteam == "Mets":
-                with col5:
-                    from PIL import Image 
-                    mets = Image.open('Images/mets.png')
-                    st.image(mets)
-            elif nlteam == "Nationals":
-                with col5:
-                    from PIL import Image 
-                    nationals = Image.open('Images/nationals.png')
-                    st.image(nationals)
-            elif nlteam == "Phillies": 
-                with col5:
-                    from PIL import Image 
-                    phillies = Image.open('Images/phillies.jpeg')
-                    st.image(phillies)
-                    
-            elif nlteam == "Brewers":
-                with col5:
-                    from PIL import Image 
-                    brewers = Image.open('Images/brewers.png')
-                    st.image(brewers)
-                    
-            elif nlteam == "Cardinals":
-                with col5:
-                    from PIL import Image 
-                    cards = Image.open('Images/cardinals.png')
-                    st.image(cards)
-            elif nlteam == "Cubs":
-                with col5:
-                    from PIL import Image 
-                    cubs = Image.open('Images/cubs.png')
-                    st.image(cubs)
-            elif nlteam == "Pirates":
-                with col5:
-                    from PIL import Image 
-                    pit = Image.open('Images/pit.png')
-                    st.image(pit)
-            elif nlteam == "Reds":
-                with col5:
-                    from PIL import Image 
-                    reds = Image.open('Images/reds.webp')
-                    st.image(reds)
-            elif nlteam == "D-backs":
-                with col5:
-                    from PIL import Image 
-                    dbacks = Image.open('Images/dbacks.png')
-                    st.image(dbacks)
-            elif nlteam == "Dodgers":
-                with col5:
-                    from PIL import Image 
-                    lad = Image.open('Images/laa.png')
-                    st.image(lad)
-            elif nlteam == "Dodgers":
-                with col5:
-                    from PIL import Image 
-                    lad = Image.open('Images/laa.png')
-                    st.image(lad)
-            elif nlteam == "Giants":
-                with col5:
-                    st.markdown(" ")
-                    from PIL import Image 
-                    g = Image.open('Images/giants.jpeg')
-                    st.image(g)
-            elif nlteam == "Padres":
-                with col5:
-                    from PIL import Image 
-                    sd = Image.open('Images/sd.png')
-                    st.image(sd)
-            elif nlteam == "Rockies":
-                with col5:
-                    from PIL import Image 
-                    col = Image.open('Images/col.png')
-                    st.image(col)
-    else:
-        alteam = st.radio("Select an American League Team.", ('Blue Jays', 'Orioles', 'Rays', 'Red Sox', 'Yankees', 'Guardians', 'Royals', 'Tigers', 'Twins', 'White Sox', 'Angels', 'Astros', 'Athletics', 'Mariners', 'Rangers'))
-        if alteam == "Blue Jays":
-            hitter = st.selectbox("Please select a hitter.", ['Bo Bichette: TOR (SS/R)', 'Vladimir Guerrero Jr.: TOR (1B/R)', 'Teoscar Hernandez: TOR (RF/R)'])
-            if hitter == "Bo Bichette":
-                hitter = 666182
-            elif hitter == "Vladimir Guerrero Jr.":
-                hitter = 665489
-            else:
-                hitter = 606192
-        '''
-        player_name = st.text_input('Enter last name of player:')
-
-        if player_name:
-            player_info = playerid_lookup(player_name)
-            st.write(player_info)
-            plot = st.text_input("Enter player's key_mlbam:")
-            if plot:
-                data = statcast_batter('2022-04-07', '2022-10-02', plot)
-                s = spraychart(data, 'generic', title = player_name)
-                fig = s.figure
-                # Display the spraychart
-                st.pyplot(fig)
-                tot = data.events.value_counts()
-                st.write(tot)
-                fig2 = px.bar(data, x = 'events', y = "pitch_type", animation_frame = "game_date", animation_group = "events")
-                st.write(fig2)
-        '''
+     
         first_name = st.text_input('Enter a players first name:')
         last_name = st.text_input('Enter a players last name:')
 
@@ -265,6 +149,106 @@ def page3():
     
     
     names = ['Double', 'Field Error', 'Field Out', 'Hit by Pitch', 'Home Run', 'Single', 'Strikeout', 'Triple', 'Walk']
+    
+        league = st.radio("Select National League or American League:", ("NL", "AL"))
+    if league == "NL":
+        col4, col5, col6 = st.columns(3)
+        with col4:
+            st.write("You selected the National League.")
+            nlteam = st.radio("Select a National League Team.", ('Braves', 'Marlins', 'Mets', 'Nationals', 'Phillies', 'Brewers', 'Cardinals', 'Cubs', 'Pirates', 'Reds', 'D-backs', 'Dodgers', 'Giants', 'Padres', 'Rockies'))
+            if nlteam == "Braves":
+                with col5:
+                    from PIL import Image 
+                    braves = Image.open('Images/braves logo.png')
+                    st.image(braves)
+            elif nlteam == "Marlins":
+                with col5:
+                    from PIL import Image 
+                    marlins = Image.open('Images/marlins.png')
+                    st.image(marlins)
+            elif nlteam == "Mets":
+                with col5:
+                    from PIL import Image 
+                    mets = Image.open('Images/mets.png')
+                    st.image(mets)
+            elif nlteam == "Nationals":
+                with col5:
+                    from PIL import Image 
+                    nationals = Image.open('Images/nationals.png')
+                    st.image(nationals)
+            elif nlteam == "Phillies": 
+                with col5:
+                    from PIL import Image 
+                    phillies = Image.open('Images/phillies.jpeg')
+                    st.image(phillies)
+                    
+            elif nlteam == "Brewers":
+                with col5:
+                    from PIL import Image 
+                    brewers = Image.open('Images/brewers.png')
+                    st.image(brewers)
+                    
+            elif nlteam == "Cardinals":
+                with col5:
+                    from PIL import Image 
+                    cards = Image.open('Images/cardinals.png')
+                    st.image(cards)
+            elif nlteam == "Cubs":
+                with col5:
+                    from PIL import Image 
+                    cubs = Image.open('Images/cubs.png')
+                    st.image(cubs)
+            elif nlteam == "Pirates":
+                with col5:
+                    from PIL import Image 
+                    pit = Image.open('Images/pit.png')
+                    st.image(pit)
+            elif nlteam == "Reds":
+                with col5:
+                    from PIL import Image 
+                    reds = Image.open('Images/reds.webp')
+                    st.image(reds)
+            elif nlteam == "D-backs":
+                with col5:
+                    from PIL import Image 
+                    dbacks = Image.open('Images/dbacks.png')
+                    st.image(dbacks)
+            elif nlteam == "Dodgers":
+                with col5:
+                    from PIL import Image 
+                    lad = Image.open('Images/laa.png')
+                    st.image(lad)
+            elif nlteam == "Dodgers":
+                with col5:
+                    from PIL import Image 
+                    lad = Image.open('Images/laa.png')
+                    st.image(lad)
+            elif nlteam == "Giants":
+                with col5:
+                    st.markdown(" ")
+                    from PIL import Image 
+                    g = Image.open('Images/giants.jpeg')
+                    st.image(g)
+            elif nlteam == "Padres":
+                with col5:
+                    from PIL import Image 
+                    sd = Image.open('Images/sd.png')
+                    st.image(sd)
+            elif nlteam == "Rockies":
+                with col5:
+                    from PIL import Image 
+                    col = Image.open('Images/col.png')
+                    st.image(col)
+    else:
+        alteam = st.radio("Select an American League Team.", ('Blue Jays', 'Orioles', 'Rays', 'Red Sox', 'Yankees', 'Guardians', 'Royals', 'Tigers', 'Twins', 'White Sox', 'Angels', 'Astros', 'Athletics', 'Mariners', 'Rangers'))
+        if alteam == "Blue Jays":
+            hitter = st.selectbox("Please select a hitter.", ['Bo Bichette: TOR (SS/R)', 'Vladimir Guerrero Jr.: TOR (1B/R)', 'Teoscar Hernandez: TOR (RF/R)'])
+            if hitter == "Bo Bichette":
+                hitter = 666182
+            elif hitter == "Vladimir Guerrero Jr.":
+                hitter = 665489
+            else:
+                hitter = 606192
     
     def predict(inputs):
         inputs = s.transform(inputs)
