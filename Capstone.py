d51f8884c02d9f07caa7a834bf9233c2a41d1481
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-14 11:46:02 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -32,8 +32,9 @@ def main_page():
         st.image(image5)
     with col3:
         st.image(image4)
-    
-    st.markdown("Do you want to predict moments like this? Continue to the other pages.")
+        
+    st.write(" ")
+    st.markdown("### Do you want to predict moments like this? Continue to the other pages.")
     st_player("https://www.youtube.com/watch?v=clDXWm1jpfY")
 
     st.sidebar.markdown("# Welcome!⚾️")
