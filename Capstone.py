f0e3d70c048d94ac1dae967d15fc9762b65a789b
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-14 22:12:41 -0800

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -251,7 +251,7 @@ def page3():
     elif hitter == 'Sean Murphy: OAK (C/R)':
         hitter = 669221
   
-    pitcherdiv = st.radio("Select pitcher from the AL", ('AL'))
+    pitcherdiv = st.checkbox("Select pitcher from the AL", ('AL'))
     if pitcherdiv == 'AL':
         pitcheral = st.selectbox("Please select an AL pitcher.", ['Tyler Wells: BAL (R)', 'Drew Rasmussen: TB (R)', 'Nick Pivetta: BOS (R)', 'Nestor Cortes: NYY (L)', 'Triston McKenzie: CLE (R)', 'Brady Singer: KC (R)', 'Tarik Skubal: DET (L)', 'Sonny Gray: MIN (R)', 'Dylan Cease: CWS (R)', 'Shohei Ohtani: LAA (R)', 'Justin Verlander: HOU (R)', 'James Kaprielian: OAK (R)', 'Chris Flexen: SEA (R)', 'Martin Perez: TEX (L)'])
         if pitcheral == 'Tyler Wells: BAL (R)':
