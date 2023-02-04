aed909b1ee9a36b9a4d6947ca40b297d4bc8f92b
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-02-04 15:21:09 -0600

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -60,7 +60,6 @@ def main_page():
         assert st_player("https://www.youtube.com/watch?v=clDXWm1jpfY", st.write("This is the actual information hereheheheheh")), "Fail!!!"
 
         # Test if the link to the video is displayed
-        st.write("Source: https://www.youtube.com/watch?v=clDXWm1jpfY")
         assert st.write("Source: https://www.youtube.com/watch?v=clDXWm1jpfY"), "Failed to display link to video"
     test_youtube_video()
 
