947a7e89a5c029b448e42271f9e41fc8749ea3fb
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-02-04 16:44:42 -0600

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -60,6 +60,7 @@ def page2():
     st.title("Explore MLB Data and Visualize Spraycharts of your Favorite Players ⚾️") 
     st.markdown("#### Enter players from the 2022 season only!") #only 2022 players should be entered
     #try code for entering player names and displaying a spray chart plot
+    from pybaseball import playerid_lookup
     first_name = st.text_input('Enter a players first name:')
     first_name = first_name.strip()
     if " " in first_name:
